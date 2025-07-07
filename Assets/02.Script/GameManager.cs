37fb628aefe7196402b4d448282dbcf72d92d313
이태환 <sheena1131@kakao.com>
2025-07-07 17:53:31 +0900

    [이태환] 중간점검

--- Assets/02.Script/GameManager.cs
+++ Assets/02.Script/GameManager.cs
@@ -1,14 +1,43 @@
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public static GameManager Instance { get; private set; }
+
+    public int favor = 0;
+    public int gold = 0;
+    public int people = 1;
+
+
+    public TMP_Text favorText;
+    public TMP_Text goldText;
+    public TMP_Text peopleText;
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
     void Start()
     {
-        
+        RefreshUI(); 
     }
+    void RefreshUI()
+        {
+            RefreshFavor();
+            RefreshGold();
+            RefreshPeople();
+        }
 
-    // Update is called once per frame
+        void RefreshFavor()  { if (favorText  != null) favorText.text  = favor .ToString("N0"); }
+        void RefreshGold()   { if (goldText   != null) goldText .text  = gold  .ToString("N0"); }
+        void RefreshPeople() { if (peopleText != null) peopleText.text = people.ToString("N0"); }
+
     void Update()
     {
         
